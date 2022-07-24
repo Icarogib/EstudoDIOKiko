4eb14bcd6b160d08283b707ecee22bae23e1a223
Icarogib <icarogib@gmail.com>
2022-07-24 13:58:20 -0300

    base DOM, events e outros

---
+++ JavaScript/Programacao/modulos e classes/DOM/assets/js/scripts.js
@@ -0,0 +1,10 @@
+const modeButton = document.getElementById('mode-selector');
+const h1bod = document.getElementById('page-title');
+const myBody = document.getElementsByTagName('body')[0];
+const myFooter = document.getElementsByTagName('footer')[0];
+
+modeButton.addEventListener('click', changeMode);
+
+function changeMode(){
+	console.log('Cliquei!');
+}
