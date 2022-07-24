eaf144a42393e45aaa645d6d4492c55ec4fca8e2
Icarogib <icarogib@gmail.com>
2022-07-24 15:24:08 -0300

    Finalizado Javascript

--- JavaScript/Programacao/modulos e classes/DOM/assets/js/scripts.js
+++ JavaScript/Programacao/modulos e classes/DOM/assets/js/scripts.js
@@ -6,5 +6,24 @@ const myFooter = document.getElementsByTagName('footer')[0];
 modeButton.addEventListener('click', changeMode);
 
 function changeMode(){
-	changeClasses();
+	changeClass();
+	changeText();
+}
+
+function changeClass(){
+	modeButton.classList.toggle("dark-mode");
+	h1bod.classList.toggle("dark-mode");
+	myBody.classList.toggle("dark-mode");
+	myFooter.classList.toggle("dark-mode");
+}
+
+function changeText(){
+	if(modeButton.classList.contains("dark-mode")){
+		modeButton.innerHTML = "Light Mode";
+		h1bod.innerHTML = "Dark Mode ON";
+		return;
+	}
+
+	modeButton.innerHTML = "Dark Mode";
+	h1bod.innerHTML = "Light Mode ON";
 }
